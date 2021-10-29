0428738d4a04d6ef7b96a44ee611f82996cfb5a7
Sergeev David Evgenievich <hwax0r@gmail.com>
2021-10-29 19:57:49 +0300

    guess what

--- Guess what/main.swift
+++ Guess what/main.swift
@@ -7,5 +7,25 @@
 
 import Foundation
 
-print("Hello, World!")
+let randomNumber = UInt.random(in: 1...100)
 
+print("Now we have a random integer in Range [1,100].")
+print("Guess it!")
+
+while true {
+    print("Your number is:")
+    guard let userNumber = UInt8(readLine() ?? "") else {
+        print("Wrong input! Try one more time.\n")
+        continue
+    }
+    if userNumber == randomNumber {
+        print("\n******************")
+        print("**** WINNER!!!****")
+        print("******************\n")
+        break
+    } else if userNumber > randomNumber {
+        print("Your number is bigger\n")
+    } else if userNumber < randomNumber {
+        print("Your number is lower\n")
+    }
+}
